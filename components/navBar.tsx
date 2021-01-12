c9f6fe239c864565713fda70a8ea95d876d7bcf4
Afiz Savage <savageafiz9@gmail.com>
2021-01-12 09:15:52 +0000

    navbar links

--- components/navBar.tsx
+++ components/navBar.tsx
@@ -1,17 +1,17 @@
 const Navbar = (params) => {
   return (
     <nav className="bg-white py-5">
-      <div className="max-w-7xl mx-auto px- sm:px-6 lg:px-8">
-        <div className="relative flex items-center justify-between h">
+      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+        <div className="relative flex items-center justify-between h-16">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-            {/* Mobile menu button */}
+            {/* <!-- Mobile menu button--> */}
             <button
               className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
               aria-expanded="false"
             >
               <span className="sr-only">Open main menu</span>
               {/* <!-- Icon when menu is closed. --> */}
-              {/* Heroicon name: menu Menu open: "hidden", Menu closed: "block" */}
+              {/* <!--Heroicon name: menuMenu open: "hidden", Menu closed: "block"--> */}
               <svg
                 className="block h-6 w-6"
                 xmlns="http://www.w3.org/2000/svg"
@@ -39,7 +39,7 @@ const Navbar = (params) => {
               >
                 <path
                   stroke-linecap="round"
-                  strokeLinejoin="round"
+                  stroke-linejoin="round"
                   stroke-width="2"
                   d="M6 18L18 6M6 6l12 12"
                 />
@@ -56,7 +56,7 @@ const Navbar = (params) => {
               <img
                 className="hidden lg:block h-8 w-auto"
                 src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
-                alt="Workflow "
+                alt="Workflow"
               />
             </div>
             <div className="hidden sm:block sm:ml-6">
@@ -78,14 +78,40 @@ const Navbar = (params) => {
                   href="#"
                   className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                 >
+                  Projects
+                </a>
+                <a
+                  href="#"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
                   Calendar
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+            {/* <!-- Profile dropdown --> */}
+            <a
+              href="#"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+            >
+              Sign In
+            </a>
+            <div className="ml-3 relative">
+              <div>
+                <a
+                  href="#"
+                  className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Sign Up
                 </a>
               </div>
             </div>
           </div>
         </div>
       </div>
-      {/* <!--Mobile menu toggle classes based on menu state Menu open: "block", Menu closed: "hidden"-> */}
+
+      {/* <!--Mobile menu, toggle classes based on menu state.Menu open: "block", Menu closed: "hidden"--> */}
       <div className="hidden sm:hidden">
         <div className="px-2 pt-2 pb-3 space-y-1">
           {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
