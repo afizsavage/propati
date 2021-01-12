a1390cacaf11f782c2a7cd692e336b3a548f2b43
change <yodusav@gmail.com>
2021-01-12 15:51:59 +0000

    navbar links named

--- components/navBar.tsx
+++ components/navBar.tsx
@@ -1,6 +1,6 @@
 const Navbar = (params) => {
   return (
-    <nav className="bg-white py-5">
+    <nav className="bg-white py-1">
       <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
         <div className="relative flex items-center justify-between h-16">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
@@ -40,7 +40,7 @@ const Navbar = (params) => {
                 <path
                   stroke-linecap="round"
                   stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeWidth="2"
                   d="M6 18L18 6M6 6l12 12"
                 />
               </svg>
@@ -63,28 +63,28 @@ const Navbar = (params) => {
               <div className="flex space-x-4">
                 {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
                 <a
-                  href="#"
-                  className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
+                  href="home"
+                  className=" text-purple-700 px-3 py-2 text-sm font-medium"
                 >
-                  Dashboard
+                  Home
                 </a>
                 <a
                   href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  className="text-purple-700 hover:text-white px-3 py-2  text-sm font-medium"
                 >
-                  Team
+                  Buy
                 </a>
                 <a
                   href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  className="text-purple-700 hover:text-white px-3 py-2  text-sm font-medium"
                 >
-                  Projects
+                  Sell
                 </a>
                 <a
                   href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  className="text-purple-700 hover:text-white px-3 py-2  text-sm font-medium"
                 >
-                  Calendar
+                  About Us
                 </a>
               </div>
             </div>
@@ -93,15 +93,15 @@ const Navbar = (params) => {
             {/* <!-- Profile dropdown --> */}
             <a
               href="#"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+              className="text-gray-300  hover:text-white px-3 py-2 text-sm font-medium"
             >
               Sign In
             </a>
-            <div className="ml-3 relative">
+            <div className="ml-3 hidden sm:block relative">
               <div>
                 <a
                   href="#"
-                  className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
+                  className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium "
                 >
                   Sign Up
                 </a>
@@ -119,25 +119,25 @@ const Navbar = (params) => {
             href="#"
             className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
           >
-            Dashboard
+            Home
           </a>
           <a
             href="#"
             className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
           >
-            Team
+            Buy
           </a>
           <a
             href="#"
             className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
           >
-            Projects
+            Sell
           </a>
           <a
             href="#"
             className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
           >
-            Calendar
+            About Us
           </a>
         </div>
       </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,8 @@ export default function Home() {
   return (
     <div>
       <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Find</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
