9c2a6b177a46ff164cf3ffeeb3c16b27bdd4f834
Afiz Savage <savageafiz9@gmail.com>
2021-01-16 13:08:49 +0000

    obile menu button setup

--- components/navBar.tsx
+++ components/navBar.tsx
@@ -2,13 +2,12 @@ import React, { useState, FC, ReactElement } from "react";
 
 const Navbar: FC = (params): ReactElement => {
   const [menuOpen, setMenuOpen] = useState(false);
-  const openMenu = (params) => {
+  const toggleMenu = (params) => {
     menuOpen === false ? setMenuOpen(true) : setMenuOpen(false);
     let bars = document.getElementById("menuBtn").children;
     Array.from(bars).forEach((bar) => {
-      bar.classList.toggle("change");
+      bar.classList.toggle("hidden");
     });
-    console.log(bars);
   };
   return (
     <nav className="bg-white py-1">
@@ -18,13 +17,45 @@ const Navbar: FC = (params): ReactElement => {
             {/* <!-- Mobile menu button--> */}
             <button
               id="menuBtn"
-              onClick={openMenu}
-              className="inline-block justify-center p-2  text-gray-400 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+              onClick={toggleMenu}
+              className="inline-flex justify-center p-2  text-gray-400 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
               aria-expanded="false"
             >
-              <div className="ar1"></div>
-              <div className="ar2"></div>
-              <div className="ar3"></div>
+              <span className="sr-only">Open main menu</span>
+              {/* <!-- Icon when menu is closed. --> */}
+              {/* <!-- Heroicon name: menuMenu open: "hidden", Menu closed: "block"--> */}
+              <svg
+                className="block h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+              {/* <!-- Icon when menu is open. --> */}
+              {/* <!--Heroicon name: xMenu open: "block", Menu closed: "hidden"--> */}
+              <svg
+                className="hidden h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
             </button>
           </div>
           <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
