ea3a10aa1b57d55a2003d63e2c89b76756f5392f
change <yodusav@gmail.com>
2021-01-18 13:39:22 +0000

    footer copyright color added

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,10 +1,22 @@
 const Footer = (params) => {
   return (
-    <div className=" flex justify-center">
-      <span className="">
-        © {new Date().getFullYear()} Propati, Inc. All Right Reserved.
-      </span>
-    </div>
+    <footer className="bg-gray-100 flex px-20">
+      <div className="w-full py-6 text-gray-900 text-sm font-light">
+        <span className="">
+          © {new Date().getFullYear()} Propati, Inc. All Right Reserved.
+        </span>
+        <span>
+          <a className="px-5" href="#">
+            Privacy.
+          </a>
+        </span>
+        <span>
+          <a className="" href="#">
+            Terms.
+          </a>
+        </span>
+      </div>
+    </footer>
   );
 };
 
