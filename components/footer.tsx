1e0181b30682f9e4543281466d012d7077c81a4f
change <yodusav@gmail.com>
2021-01-19 19:11:01 +0000

    footer spacing and responsiveness arranged

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,9 +4,9 @@ import Link from "next/link";
 
 const Footer = (params) => {
   return (
-    <footer className="bg-gray-100 flex flex-col px-8 sm:px-20">
-      <div className="flex flex-col md:flex-row py-7 md:justify-between text-left">
-        <div className="md:w-1/4">
+    <footer className="bg-gray-100 flex flex-col px-8 sm:px-20 border-gray-300 border-t md:border-none">
+      <div className="flex flex-col md:flex-row py-8 md:py-10 md:justify-between text-left">
+        <div className="footerCols">
           <h3 className="font-semibold text-sm">Company</h3>
           <ul className="font-normal text-sm">
             <li className="mt-3">
@@ -14,18 +14,59 @@ const Footer = (params) => {
                 <a>About Us</a>
               </Link>
             </li>
+            <li className="mt-3">
+              <Link href="#">
+                <a>FAQ's</a>
+              </Link>
+            </li>
           </ul>
         </div>
-        <div className="md:w-1/4">
+        <div className="footerCols">
           <h3 className="font-semibold text-sm">For Tenants</h3>
+          <ul className="font-normal text-sm">
+            <li className="mt-3">
+              <Link href="#">
+                <a>Apply Online</a>
+              </Link>
+            </li>
+            <li className="mt-3">
+              <Link href="#">
+                <a> Schedule Tour</a>
+              </Link>
+            </li>
+          </ul>
         </div>
-        <div className="md:w-1/4">
+        <div className="footerCols ">
           {" "}
           <h3 className="font-semibold text-sm">For Landlords</h3>
+          <ul className="font-normal text-sm">
+            <li className="mt-3">
+              <Link href="#">
+                <a>List A Property</a>
+              </Link>
+            </li>
+            <li className="mt-3">
+              <Link href="#">
+                <a>Arrange Online Tour</a>
+              </Link>
+            </li>
+          </ul>
         </div>
-        <div className="md:w-1/4">
+        <div className="md:w-1/4 ">
           {" "}
           <h3 className="font-semibold text-sm">Work With Us</h3>
+          <ul className="font-normal text-sm">
+            <li className="mt-3">
+              <Link href="#">
+                <a>For Partners</a>
+              </Link>
+            </li>
+            <li className="mt-3">
+              <Link href="#">
+                <a>Advertise With Us</a>
+              </Link>
+            </li>
+          </ul>
         </div>
       </div>
       <div className="flex w-full col-span-full py-6 text-gray-900 border-gray-300 border-t text-sm font-light justify-center sm:justify-between">
@@ -46,20 +87,20 @@ const Footer = (params) => {
             </span>
           </div>
         </div>
-        <div className="sm:flex d- hidden">
+        <div className="md:flex d- hidden">
           <span>
             <Link href="#" passHref={true}>
-              <ImFacebook size={18} />
+              <ImFacebook className="hover:text-gray-600" size={18} />
             </Link>
           </span>
           <span className="px-6">
             <Link href="#" passHref={true}>
-              <ImTwitter size={18} />
+              <ImTwitter className="hover:text-gray-600" size={18} />
             </Link>
           </span>
           <span>
             <Link href="#" passHref={true}>
-              <ImInstagram size={18} />
+              <ImInstagram className="hover:text-gray-600" size={18} />
             </Link>
           </span>
         </div>
