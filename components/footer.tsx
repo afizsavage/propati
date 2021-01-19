0b7a2f56587cd4218ede1d560a7745e792dbfb42
change <yodusav@gmail.com>
2021-01-19 17:17:42 +0000

    top footer responsiveness arranged

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,20 +5,27 @@ import Link from "next/link";
 const Footer = (params) => {
   return (
     <footer className="bg-gray-100 flex flex-col px-8 sm:px-20">
-      <div className="flex flex-row py-7 justify-between">
-        <div>
-          <h3>Company</h3>
+      <div className="flex flex-col md:flex-row py-7 md:justify-between text-left">
+        <div className="md:w-1/4">
+          <h3 className="font-semibold text-sm">Company</h3>
+          <ul className="font-normal text-sm">
+            <li className="mt-3">
+              <Link href="#">
+                <a>About Us</a>
+              </Link>
+            </li>
+          </ul>
         </div>
-        <div>
-          <h3>For Tenants</h3>
+        <div className="md:w-1/4">
+          <h3 className="font-semibold text-sm">For Tenants</h3>
         </div>
-        <div>
+        <div className="md:w-1/4">
           {" "}
-          <h3>For Landlords</h3>
+          <h3 className="font-semibold text-sm">For Landlords</h3>
         </div>
-        <div>
+        <div className="md:w-1/4">
           {" "}
-          <h3>Work With Us</h3>
+          <h3 className="font-semibold text-sm">Work With Us</h3>
         </div>
       </div>
       <div className="flex w-full col-span-full py-6 text-gray-900 border-gray-300 border-t text-sm font-light justify-center sm:justify-between">
