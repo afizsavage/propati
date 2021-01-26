959fec1e28efcf3793642c5752229f033a45d9a5
change <yodusav@gmail.com>
2021-01-26 19:32:51 +0000

    merge conflict commit

--- components/heroSection.tsx
+++ components/heroSection.tsx
@@ -1,6 +1,5 @@
 const HeroSection = (params) => {
   return (
-<<<<<<< HEAD
     <div className="md:h-96 h-auto bg-gray-50 grid w-full">
       <div className="grid grid-cols-1 md:grid-cols-2 md:px-12 px-6">
         <div className="my-auto">
@@ -12,16 +11,6 @@ const HeroSection = (params) => {
           </div>
         </div>
         <div className="bg-center h-96 md:h-auto order-first md:order-2 bg-hero-image bg-contain bg-no-repeat"></div>
-=======
-    <div className="h-96 bg-gray-50 grid w-full">
-      <div className="grid grid-cols-1 md:grid-cols-2 px-12">
-        <div className="my-auto">
-          <div className="mx-auto text-center md:text-left text-4xl md:text-6xl text-gray-800 font-extrabold">
-            Find the right home at your own time
-          </div>
-        </div>
-        <div className="bg-center h-80 md:h-auto order-first md:order-2 bg-hero-image bg-contain bg-no-repeat"></div>
->>>>>>> f7675b5330c787a46ac57ad4a301165ce7fec4f7
       </div>
     </div>
   );
