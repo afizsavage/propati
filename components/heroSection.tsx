13d5f1c67e9a4880b00516b0e18ffbcbd811e230
change <yodusav@gmail.com>
2021-02-01 17:31:12 +0000

    button added to hero setion

--- components/heroSection.tsx
+++ components/heroSection.tsx
@@ -1,13 +1,19 @@
 const HeroSection = (params) => {
   return (
     <div className="md:h-100 h-auto bg-gray-100 grid w-full">
-      <div className="grid grid-cols-1 md:grid-cols-2 md:px-16 px-6 ">
-        <div className="my-auto">
-          <div className="font-Oswald mx-auto text-center md:text-left text-2xl md:text-5xl text-gray-800 font-extrabold">
+      <div className="grid grid-cols-1 md:grid-cols-2 md:px-16 px-6 py-10 md:py-0 ">
+        <div className="my-auto h-80 md:h-auto">
+          <h1 className="font-Oswald mx-auto text-center md:text-left text-2xl md:text-5xl text-gray-800 font-extrabold">
             The ideal renting and buying solution
+          </h1>
+          <p className="py-4"></p>
+          <div className="flex justify-center  md:justify-start w-full">
+            <a href="#" className="primeBtn">
+              Explore Homes
+            </a>
           </div>
         </div>
-        <div className="bg-center h-96 md:h-auto order-first md:order-2 bg-hero-image bg-contain bg-no-repeat"></div>
+        <div className="bg-center h-80 md:h-auto order-first md:order-2 bg-hero-image bg-contain bg-no-repeat"></div>
       </div>
     </div>
   );
