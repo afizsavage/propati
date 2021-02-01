b370e97229078664be7792f42c157078cd37a23f
change <yodusav@gmail.com>
2021-02-01 19:33:20 +0000

    leading added to hero section heading

--- components/heroSection.tsx
+++ components/heroSection.tsx
@@ -3,7 +3,7 @@ const HeroSection = (params) => {
     <div className="md:h-100 h-auto bg-gray-100 grid w-full">
       <div className="grid grid-cols-1 md:grid-cols-2 md:px-16 px-6 py-10 md:py-0 ">
         <div className="my-auto h-80 md:h-auto">
-          <h1 className="font-Oswald mx-auto text-center md:text-left text-2xl md:text-5xl text-gray-800 font-extrabold">
+          <h1 className="font-Oswald mx-auto text-center md:text-left text-2xl md:text-5xl md:leading-tight text-gray-800 font-extrabold">
             The ideal renting and buying solution
           </h1>
           <p className="py-4"></p>
