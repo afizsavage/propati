fdac3f1ca63da491babd7c7806711ed1adce0dd6
Afiz Savage <savageafiz9@gmail.com>
2021-02-15 08:22:46 +0000

    filter section filter button icon inserted

--- components/filter.tsx
+++ components/filter.tsx
@@ -1,10 +1,20 @@
 import React, { useState, FC, ReactElement, useRef, useEffect } from "react";
-import { Z_BLOCK } from "zlib";
 
 const ListingsFilter: FC = (params): ReactElement => {
   return (
     <div className="inline-block w-20">
-      <button type="button" className="filterBtn">Filters</button>
+      <button type="button" className="filterBtn">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          className="h-5 w-5 text-gray-500 mr-2"
+          viewBox="0 0 16 16"
+          aria-hidden="true"
+        >
+          <path d="M6 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z" />
+        </svg>
+        Filters
+      </button>
     </div>
   );
 };
