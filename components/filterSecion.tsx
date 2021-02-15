591594ab19023135ee517ebc37a5c5d6b0797d5b
change <yodusav@gmail.com>
2021-02-15 19:47:53 +0000

    search icon added on searh location input in filter section

--- components/filterSecion.tsx
+++ components/filterSecion.tsx
@@ -121,15 +121,29 @@ const ItemsFilter = (params) => {
     <div className="pt-7">
       <form className="w-full space-y-4 text-gray-700">
         <div className="flex flex-wrap -mx-2 space-y-4 md:space-y-0">
-          <div className="w-full px-4 md:w-1/5">
-            <label className="block mb-2 text-gray-800 font-bold  " htmlFor="formGridCode_month">
+          <div className="relative w-full px-4 md:w-1/5">
+            <label
+              className="block mb-2 text-gray-800 font-bold  "
+              htmlFor="formGridCode_month"
+            >
               Location
             </label>
             <input
-              className="w-full h-10 px-3 text-base placeholder-gray-600 border rounded-lg focus:outline-none bg-gray-100 hover:bg-white hover:ring-4 ring-teal-400 ring-opacity-20"
+              className="w-full h-10 px-3 text-base placeholder-gray-600 rounded-lg focus:outline-none bg-gray-100 hover:bg-white hover:ring-4 ring-teal-400 ring-opacity-20"
               type="text"
               id="formGridCode_month"
             />
+            <div className="absolute bottom-3 left-7 flex items-center px-2 pointer-events-none">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                viewBox="0 0 16 16"
+              >
+                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+              </svg>
+            </div>
           </div>
           <div className="w-full px-4 md:w-1/5">
             <label className="block mb-2" htmlFor="formGridCode_year">
@@ -161,9 +175,7 @@ const ItemsFilter = (params) => {
               id="formGridCode_cvc"
             />
           </div>
-          <div className="w-full px-4 md:w-1/5">
-            
-          </div>
+          <div className="w-full px-4 md:w-1/5"></div>
         </div>
       </form>
     </div>
