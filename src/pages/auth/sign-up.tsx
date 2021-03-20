a1185a5077d579e9286a9f778a8e0ed5f227c6c1
Afiz Savage <savageafiz9@gmail.com>
2021-03-20 19:35:52 +0000

    register page added

--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -1,11 +1,17 @@
-import React, { useState, FC, ReactElement } from "react";
+import React, { useState } from "react";
+import { IoMdClose } from "react-icons/io";
 
 const SignUp = (params) => {
-  const [serviceType, setServiceType] = useState(null);
   return (
-    <div className="w-screen h-screen">
-      <div className="w-full flex">
-        <div className="shadow-2xl  rounded mx-auto to w-full md:w-1/3 border-solid border-t-4 border-teal-500 p-0 my-2">
+    <div className="bg-gray-100 pt-8 flex flex-col min-h-screen h-full">
+      <button className="auth-close-btn">
+        <IoMdClose className="text-3xl " />
+      </button>
+      <header className="w-full text-gray-800 flex justify-center py-5 my-1">
+        <h1 className="font-lato  text-5xl font-bold ">Sign Up</h1>
+      </header>
+      <div className="flex flex-grow flex-col">
+        <div className="shadow-2xl rounded mx-auto bg-white w-full md:w-1/3 border-solid border-t-4 border-teal-500 p-0 my-2">
           <form id="signupForm" className="">
             <div className="p-8">
               <div className="w-full input-container">
@@ -38,10 +44,24 @@ const SignUp = (params) => {
               </div>
             </div>
 
-            <button className="auth-submit">Sign Up</button>
+            <button className="auth-submit-btn">Sign Up</button>
           </form>
         </div>
       </div>
+      <footer className="flex justify-center pb-8">
+        <div className="self-center sm:inline text-sm text-gray-400">
+          <span>
+            By signing up you agree to Prɔpati's{" "}
+            <a className="text-teal-600 hover:underline" href="#">
+              Privacy Policy
+            </a>{" "}
+            and{" "}
+            <a className="text-teal-600 hover:underline" href="#">
+              Terms
+            </a>
+          </span>
+        </div>
+      </footer>
     </div>
   );
 };
