2264d776820c6bc0fa77a42fc08ff7c67d318157
Afiz Savage <savageafiz9@gmail.com>
2021-03-21 16:23:02 +0000

    register page responsive for mobile

--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -3,11 +3,11 @@ import { IoMdClose } from "react-icons/io";
 
 const SignUp = (params) => {
   return (
-    <div className="bg-gray-100 pt-8 flex flex-col min-h-screen h-full">
+    <div className="bg-gray-100 pt-9 flex flex-col min-h-screen h-full w-screen">
       <button className="auth-close-btn">
         <IoMdClose className="text-3xl " />
       </button>
-      <header className="w-full text-gray-800 flex justify-center py-5 my-1">
+      <header className="hidden lg:flex w-full text-gray-800 flex justify-center py-5 my-1">
         <h1 className="font-lato  text-5xl font-bold ">Sign Up</h1>
       </header>
       <div className="flex flex-grow flex-col">
@@ -47,16 +47,24 @@ const SignUp = (params) => {
             <button className="auth-submit-btn">Sign Up</button>
           </form>
         </div>
+        <div className="self-center sm:inline text-sm text-gray-400 mt-2">
+          <span className="transition duration-300 ease-in">
+            Already have Prɔpati account?{" "}
+            <a className="auth-links" href="#">
+              Log in
+            </a>
+          </span>
+        </div>
       </div>
       <footer className="flex justify-center pb-8">
         <div className="self-center sm:inline text-sm text-gray-400">
-          <span>
+          <span className="transition duration-300 ease-in">
             By signing up you agree to Prɔpati's{" "}
-            <a className="text-teal-600 hover:underline" href="#">
+            <a className="auth-links" href="#">
               Privacy Policy
             </a>{" "}
             and{" "}
-            <a className="text-teal-600 hover:underline" href="#">
+            <a className="auth-links" href="#">
               Terms
             </a>
           </span>
