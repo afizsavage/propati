1559ab9291c4f2166b6869b57d97061fc1acaae4
Chance <savageafiz9@gmail.com>
2021-05-31 21:48:59 +0000

    enabled ignoreBuildErrors option in next.config file

--- next.config.js
+++ next.config.js
@@ -2,4 +2,11 @@ module.exports = {
   images: {
     domains: ["picsum.photos"],
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
