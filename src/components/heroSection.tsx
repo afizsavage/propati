b9bbc156d13981191396b2ada862387772ae718e
Afiz Savage <savageafiz9@gmail.com>
2021-12-18 12:48:57 +0000

    Make search bar responsive

--- src/components/heroSection.tsx
+++ src/components/heroSection.tsx
@@ -3,20 +3,22 @@ import { CustomSelect } from './utils';
 
 const HeroSection = () => {
   const priceRanges = [
-    '$0 - $1,000',
-    '$1,000+ - $2,000',
-    '2,000+ - $3,000',
-    '3,000+ - $4,000',
-    '4,000+ - $5,000+',
+    '$0 - $4,000',
+    '$4,000+ - $8,000',
+    '8,000+ - $12,000',
+    '12,000+ - $16,000',
+    '160,000+ - $200,000+',
   ];
 
+  const listingType = ['Rent', 'Buy', 'Lease'];
+
   return (
-    <section className="md:h-200 gradient flex flex-col items-center justify-center w-full ">
-      <h1 className="text-6xl text-white font-Noto">
+    <section className="h-96 md:h-200 gradient flex flex-col items-center justify-center w-full ">
+      <h1 className="text-xl font-medium md:text-6xl text-white font-Noto">
         Finding a home should be easy.
       </h1>
       <div className="my-4">
-        <div className="bg-white flex overflow-visible items-center h-20 rounded-full">
+        <div className="w-full md:w-auto overflow-hidden bg-white flex md:overflow-visible items-center h-16 md:h-20 rounded-full">
           <div className="h-full w-72 px-7 py-0 flex flex-col hover:bg-gray-200 justify-center rounded-full">
             <label
               htmlFor="location"
@@ -29,10 +31,10 @@ const HeroSection = () => {
               name="location"
               type="text"
               placeholder="Where do you have in mind?"
-              className="focus:outline-none bg-transparent text-gray-800 font-bold active:outline-none"
+              className="focus:outline-none bg-transparent text-gray-700 font-bold active:outline-none"
             />
           </div>
-          <div className="relative h-full flex flex-col justify-center w-64 py-0 px-7 hover:bg-gray-200 rounded-full">
+          <div className="hidden relative h-full md:flex flex-col justify-center w-60 py-0 px-7 hover:bg-gray-200 rounded-full">
             <label
               htmlFor="location"
               className="block font-semibold text-gray-400"
@@ -41,23 +43,23 @@ const HeroSection = () => {
             </label>
             <CustomSelect
               options={priceRanges}
-              selectStyle="static inline-block h-auto w-full text-base text-gray-800 font-bold"
+              selectStyle="static inline-block h-auto w-full text-base text-gray-700 font-bold"
               optionsStyle="absolute bg-white h-cus w-full z-20 "
               setValue={true}
             />
           </div>
-          <div className="h-full w-48 px-7 py-0 flex flex-col hover:bg-gray-200 justify-center rounded-full">
+          <div className="w-30 relative h-full flex flex-col justify-center md:w-60 py-0 px-7 hover:bg-gray-200 rounded-full">
             <label
               htmlFor="location"
               className="block font-semibold text-gray-400"
             >
-              Listing Type
+              List Type
             </label>
-            <input
-              id="location"
-              name="location"
-              type="text"
-              className="focus:outline-none bg-transparent text-gray-800 font-bold capitalize active:outline-none"
+            <CustomSelect
+              options={listingType}
+              selectStyle="static inline-block h-auto w-full text-base text-gray-700 font-bold"
+              optionsStyle="absolute bg-white h-cus w-full z-20 "
+              setValue={true}
             />
           </div>
           <button className="h-10 w-10 text-2xl text-white flex justify-center items-center rounded-full bg-sprite mx-4">
